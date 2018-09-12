61d11955eb748c4ab7fa26e688b42cd19f4797c1
aglaya <aglayastunkus1@gmail.com>
2018-09-12 18:00:56 +0300

    add/delete empty spaces

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-    let n=1;
+    let n = 1;
     let resPow = 0;
     let pow;
-
     do {
-        pow = Math.floor(number/Math.pow(5,n));
-
+        pow = Math.floor(number/Math.pow(5, n));
         n++;
-
         resPow += pow;
-
     } while(pow > 0);
-
     return resPow;
-}
+};
